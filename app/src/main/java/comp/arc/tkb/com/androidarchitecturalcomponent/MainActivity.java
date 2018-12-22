0c48945ff1218493eeeac245e1900a53909ba39b
Tarun <tarun.pstu@gmail.com>
2018-12-23 01:11:26 +0600

    - Configured gradle and implemented sample viewModel . - Configured AndroidX

--- app/src/main/java/comp/arc/tkb/com/androidarchitecturalcomponent/MainActivity.java
+++ app/src/main/java/comp/arc/tkb/com/androidarchitecturalcomponent/MainActivity.java
@@ -1,13 +1,36 @@
 package comp.arc.tkb.com.androidarchitecturalcomponent;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    protected TextView clickCountText;
+    protected Button increment_button;
+    private ClickCounterViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickCountText = findViewById(R.id.click_count_text);
+
+        viewModel = ViewModelProviders.of(this).get(ClickCounterViewModel.class);
+        displayClickCount(viewModel.getCount());
+    }
+
+    public void incrementClickCount(View button) {
+        viewModel.setCount(viewModel.getCount() + 1);
+        displayClickCount(viewModel.getCount());
+    }
+
+    private void displayClickCount(int clickCount) {
+        clickCountText.setText(String.valueOf(clickCount));
     }
 }
